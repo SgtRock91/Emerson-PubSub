34e7d44410f185be5846ee59c2d0f7891f49c9af
Alex Gillette <alexgillette1991@gmail.com>
2022-01-09 10:26:08 -0600

    added tests for processInput

--- shared/input.test.js
+++ shared/input.test.js
@@ -1,4 +1,4 @@
-const { validateString, validateNumber } = require('./input');
+const { validateString, validateNumber, processInput } = require('./input');
 
 describe('validateString', () => {
     it('should return true if string', () => {
@@ -24,5 +24,54 @@ describe('validateNumber', () => {
         expect(validateNumber('1a1')).toEqual(false);
         expect(validateNumber(undefined)).toEqual(false);
         expect(validateNumber({})).toEqual(false);
+    });
+});
+
+//Note: can do before/afer each mock setup and resets
+describe('processInput', () => {
+    it('should call console log with help message and exit(0) on -h input', () => {
+        //build mocks
+        const realProcessExit = process.exit;
+        const realConsoleLog = console.log;
+        const mockExit = jest.fn();
+        const mockLog = jest.fn();
+
+        process.exit = mockExit;
+        console.log = mockLog;
+
+        //call
+        const msg = 'msg';
+        processInput('-h', undefined, msg);
+        expect(mockLog).toHaveBeenCalledWith(msg);
+        expect(mockExit).toHaveBeenCalledWith(0);
+
+        //reset
+        console.log = realConsoleLog;
+        process.exit = realProcessExit;
+    });
+    it('should return input when valid input given', () => {
+        const expected = '1';
+        const result = processInput('1', () => true, undefined);
+
+        expect(result).toEqual(expected);
+    });
+    it('should call console log and exit(1) when bad input given', () => {
+        //build mocks
+        const realProcessExit = process.exit;
+        const realConsoleLog = console.log;
+        const mockExit = jest.fn();
+        const mockLog = jest.fn();
+
+        process.exit = mockExit;
+        console.log = mockLog;
+
+        //call
+        processInput('1', () => false, '');
+        expect(mockLog).toHaveBeenCalled();
+        expect(mockExit).toHaveBeenCalledWith(1);
+
+        //reset
+        console.log = realConsoleLog;
+        process.exit = realProcessExit;
     });
 });
